9c8645611e98ff6cfc5c0c77887ac1945e4a60ed
Khanh Duong <dqkqdlot@gmail.com>
2026-09-19 10:43:39 +0900

    feat: add ray to hit record

--- src/Hit/HitRecord.hs
+++ src/Hit/HitRecord.hs
@@ -1,13 +1,14 @@
-module Hit.HitRecord (HitRecord (HitRecord), hitPoint, hitDistance, hitNormalVec) where
+module Hit.HitRecord (HitRecord (HitRecord), hitPoint, hitRay, hitDistance, hitNormalVec) where
 
 import Point (Point)
-import Ray (RayDistance)
+import Ray (Ray, RayDistance)
 import Vec3 (Vec3)
 
 -- | A hit data structure, contains an origin and a normal vector pointing outward
 data HitRecord
   = HitRecord
   { hitPoint :: Point
+  , hitRay :: Ray
   , hitDistance :: RayDistance
   , hitNormalVec :: Vec3 Double -- normal vector
   }
